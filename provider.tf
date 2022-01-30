901643432b2f957d0d9de585d935499056d0ce7a
Miquel Barbero <sonfrau@gmail.com>
2022-01-30 20:40:44 +0100

    Fix more errors III

--- provider.tf
+++ provider.tf
@@ -7,4 +7,12 @@ data "aws_region" "current" {}
 data "aws_availability_zones" "available" {}
 
 provider "http" {}
-provider "kubernetes" {}
+provider "kubernetes" {
+  host                   = aws_eks_cluster.seera.endpoint
+  cluster_ca_certificate = base64decode(aws_eks_cluster.seera.certificate_authority.0.data)
+  exec {
+    api_version = "client.authentication.k8s.io/v1alpha1"
+    args        = ["eks", "get-token", "--cluster-name", var.cluster-name]
+    command     = "aws"
+  }
+}
